c68c5f6aa8247729e16d438ac34d35495891b1d2
MarciaOrozco <orozcomarcia3@gmail.com>
2025-02-21 20:31:11 -0300

    p

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,23 @@
 import express, { json, Request, Response } from "express";
-import router from "./routes";
+import cors from "cors";
+import router from "./routes/index";
 import connectDB from "./database";
 import dotenv from "dotenv";
-import cors from "cors";
 
 dotenv.config();
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 connectDB();
 
-const allowedOrigins = ["https://mdw-app.vercel.app"];
-
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
-
+app.use(cors());
 app.use(json());
 app.use(router);
 
-app.get("/", (req, res) => {
-  res.send("Backend funcionando! 🚀");
-});
-
-app.use((req: Request, res: Response) => {
+app.use((_req: Request, res: Response) => {
   res.status(404).send("Route not found");
 });
 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
-});
+app.listen(port);
 
 module.exports = app;
