4a96675b239d00f206d2a57d2b5d8fcded492186
MarciaOrozco <orozcomarcia3@gmail.com>
2025-02-21 20:42:56 -0300

    a ver si es el js

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import express, { json, Request, Response } from "express";
 import cors from "cors";
-import router from "./routes/index";
-import connectDB from "./database";
+import router from "../routes/index";
+import connectDB from "../database";
 import dotenv from "dotenv";
 
 dotenv.config();
