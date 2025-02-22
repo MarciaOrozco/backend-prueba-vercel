3fedf083e7a331d4725947394d81cc29d9df0d11
MarciaOrozco <orozcomarcia3@gmail.com>
2025-02-22 09:47:13 -0300

    prueba index simple

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,24 @@
 import express, { json, Request, Response } from "express";
-import cors from "cors";
-import router from "./routes/index";
+import router from "./routes";
 import connectDB from "./database";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 
-connectDB()
-  .then(() => console.log("✅ Conectado a la base de datos"))
-  .catch((err) =>
-    console.error("❌ Error al conectar a la base de datos", err)
-  );
+connectDB();
 
 app.use(cors());
 app.use(json());
-app.use("/", router);
 
-app.get("/", (_req: Request, res: Response) => {
-  res.status(200).send("🚀 Backend funcionando en /api/");
-});
+app.use(router);
 
-app.use((_req: Request, res: Response) => {
+app.use((req: Request, res: Response) => {
   res.status(404).send("Route not found");
 });
 
-try {
-  app.listen(port, () => {
-    console.log(`🚀 Servidor corriendo en http://localhost:${port}`);
-  });
-} catch (error) {
-  console.error("❌ Error al iniciar el servidor:", error);
-}
-module.exports = app;
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
